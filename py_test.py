6b29f75007d71903e738ad427ccbff17b6614fcb
johnbomba <bomba.john@gmail.com>
2021-08-06 10:12:34 -0400

    first commit

---
+++ py_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import py_test2
+
+
+
+if __name__ == "__main__":
+    print(__name__)
---
+++ py_test2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print(__name__)
+
+if __name__ == "__main__":
+    print(__name__)
